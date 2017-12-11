28317e4220387640234a8f1f7fb3624dab6eedd7
lacoon2874 <woakd0705@naver.com>
2017-12-12 08:12:56 +0900

    최종

--- main.c
+++ main.c
@@ -1,54 +1,109 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Altino.h"
 
-#define irsensor1 170          // ï¿½âº»ï¿½ï¿½ + 13
-#define irsensor2 165          // ï¿½âº»ï¿½ï¿½
-#define irsensor3 190          // ï¿½âº»ï¿½ï¿½ + 30
-#define irsensor4 600          // ï¿½ï¿½ï¿½ï¿½ï¿½
-#define irsensor5 400
+#define irsensor1 20          // ±âº»°ª
+#define irsensor2 50          // ±âº»°ª + 13
+#define irsensor3 100          // ±âº»°ª + 30
+#define irsensor4 60          // ÃøÁ¤°ª
+#define irsensor5 100
+#define irsensor6 160
+
+#define msensor1 -1830
+#define msensor2 1220
+
+typedef struct {
+	int x;
+	int y;
+}x_y;
 
 void go(int speed);
-SensorData left(int speed, SensorData sdata);
-SensorData straight(int speed, SensorData sdata);
-SensorData right(int speed, SensorData sdata);
-void CDS(int speed, SensorData sdata);
-SensorData Back(int speed, SensorData sdata);
+void left(int speed, SensorData sdata[]);
+void straight(int speed, SensorData sdata[]);
+void right(int speed, SensorData sdata[]);
+void CDS(int speed, SensorData sdata[]);
+void Back(int speed, SensorData sdata[]);
 void Gobacksound();
-SensorData GoBack(int backSpeed, SensorData sdata);
+void Startsound();
+
+
+int map[100][100] = { 0 };
+int a, b, x = 50, y = 50;
+int count, index = 7;
+int x_go = -1, y_go = 0;
+int start, end;
+int steer;
+FILE *q, *w, *e, *r;
+
+x_y xy[8] = { { -1, 1 },{ 0, 1 },{ 1, 1 },{ 1, 0 },{ 1, -1 },{ 0, -1 },{ -1, -1 },{ -1, 0 } };
 
 int main(void) {
-	SensorData sdata;
+	SensorData sdata[2];
 
-	int speed = 300;
+	int speed = 320;
+	steer = 0;
+
+	map[x][y] = 1;
+	q = fopen("mapX.txt", "w+");
+	w = fopen("mapY.txt", "w+");
+	e = fopen("time.txt", "w+");
+	r = fopen("steer.txt", "w+");
 
 	Open(szPort);
 
 	delay(500);
 
+	sdata[0] = Sensor(1);
+	start = clock();
+	sdata[1] = Sensor(2);
+	end = clock();
+	fprintf(q, "%d\n", sdata[1].MSensor[0]);
+	fprintf(w, "%d\n", sdata[1].MSensor[1]);
+	fprintf(e, "%d\n", end - start);
+	fprintf(r, "%d\n", steer);
+
+	Startsound();
+
 	while (1) {
-		sdata = Sensor(1);
+		count = 0;
+		if (a - sdata[1].MSensor[0] <= 5 && a - sdata[1].MSensor[0] >= -5) {
+			count++;
+		}
+		if (b - sdata[1].MSensor[1] <= 5 && b - sdata[1].MSensor[1] >= -5) {
+			count++;
+		}
+		if (count == 2) {
+			x += x_go;
+			y += y_go;
+			map[x][y] = 1;
+		}
+		a = sdata[1].MSensor[0];
+		b = sdata[1].MSensor[1];
 
-		printf("%d %d %d %d\n", sdata.IRSensor[0], sdata.IRSensor[4], sdata.IRSensor[2], sdata.CDSSensor);
-		if (sdata.IRSensor[0] > irsensor3 || sdata.IRSensor[0] <= 0 || sdata.IRSensor[4] > irsensor4) {
-			sdata = Back(speed, sdata);
+		printf("%d %d %d %d\n", sdata[0].IRSensor[0], sdata[0].IRSensor[4], sdata[0].MSensor[0], sdata[0].MSensor[1]);
+		if ((sdata[0].IRSensor[0] > irsensor2 && sdata[0].IRSensor[4] > irsensor5) || sdata[0].IRSensor[0] > irsensor3) {
+			Back(speed, sdata);
 		}
-		else if (sdata.IRSensor[0] < irsensor1 && sdata.IRSensor[0] > irsensor2 && sdata.IRSensor[4] <= irsensor5) {
+		else if ((sdata[0].IRSensor[0] > irsensor1 && sdata[0].IRSensor[0] <= irsensor2) || (sdata[0].IRSensor[4] <= irsensor6 && sdata[0].IRSensor[4] > irsensor5)) {
 
-			sdata = straight(speed, sdata);
+			straight(speed, sdata);
 		}
-		else if ((sdata.IRSensor[0] >= irsensor1 && sdata.IRSensor[0] <= irsensor3) || sdata.IRSensor[0] <= 0 || (sdata.IRSensor[4] > irsensor5 && sdata.IRSensor[4] <= irsensor4)) {
-			sdata = right(speed, sdata);
+		else if ((sdata[0].IRSensor[0] > irsensor2 && sdata[0].IRSensor[0] <= irsensor3) || (sdata[0].IRSensor[4] <= irsensor5 && sdata[0].IRSensor[4] > irsensor5)) {
+			right(speed, sdata);
 		}
-		else if (sdata.IRSensor[0] <= irsensor2 && sdata.IRSensor[4] <= irsensor5) {
-			sdata = left(speed, sdata);
+		else if (sdata[0].IRSensor[0] <= irsensor1 && sdata[0].IRSensor[4] <= irsensor4) {
+			left(speed, sdata);
 		}
+
+		sdata[0] = Sensor(1);
+		sdata[1] = Sensor(2);
+		end = clock();
+		fprintf(q, "%d\n", sdata[1].MSensor[0]);
+		fprintf(w, "%d\n", sdata[1].MSensor[1]);
+		fprintf(e, "%d\n", end - start);
+		fprintf(r, "%d\n", steer);
 	}
-
-	go(0);
-	Steering(2);
-	Led(0x0000);
-
-	Close(szPort);
 
 	return 0;
 }
@@ -57,275 +112,385 @@ void go(int speed) {
 	Go(speed, speed);
 }
 
-SensorData left(int speed, SensorData sdata) {
+void left(int speed, SensorData sdata[]) {
 	Steering(1);
+	steer = 1;
+	index -= 1;
+	if (index < 0) {
+		index = 7;
+	}
+	x_go = xy[index].x;
+	y_go = xy[index].y;
 	Led(0x0020);
 	go(speed);
 
-	while (sdata.IRSensor[0] <= irsensor2 && sdata.IRSensor[4] <= irsensor5) {
-		sdata = Sensor(1);
-		printf("%d %d %d %d\n", sdata.IRSensor[0], sdata.IRSensor[4], sdata.IRSensor[2], sdata.CDSSensor);
+	while (sdata[0].IRSensor[0] <= irsensor1 && sdata[0].IRSensor[4] <= irsensor4) {
+		sdata[0] = Sensor(1);
+		sdata[1] = Sensor(2);
+		end = clock();
+		fprintf(q, "%d\n", sdata[1].MSensor[0]);
+		fprintf(w, "%d\n", sdata[1].MSensor[1]);
+		fprintf(e, "%d\n", end - start);
+		fprintf(r, "%d\n", steer);
+		count = 0;
+		if (a - sdata[1].MSensor[0] <= 5 && a - sdata[1].MSensor[0] >= -5) {
+			count++;
+		}
+		if (b - sdata[1].MSensor[1] <= 5 && b - sdata[1].MSensor[1] >= -5) {
+			count++;
+		}
+		if (count == 2) {
+			x += x_go;
+			y += y_go;
+			map[x][y] = 1;
+		}
+		a = sdata[1].MSensor[0];
+		b = sdata[1].MSensor[1];
+		printf("%d %d %d %d\n", sdata[0].IRSensor[0], sdata[0].IRSensor[4], sdata[0].MSensor[0], sdata[0].MSensor[1]);
 		CDS(speed, sdata);
 	}
 
 	Led(0x0000);
-
-	return sdata;
 }
 
-SensorData straight(int speed, SensorData sdata) {
+void straight(int speed, SensorData sdata[]) {
 	Steering(2);
+	Led(0x0002);
+	Led(0x0001);
+	steer = 0;
 	go(speed);
 
-	while (sdata.IRSensor[0] < irsensor1 && sdata.IRSensor[0] > irsensor2 && sdata.IRSensor[4] <= irsensor5) {
-		sdata = Sensor(1);
-		printf("%d %d %d %d\n", sdata.IRSensor[0], sdata.IRSensor[4], sdata.IRSensor[2], sdata.CDSSensor);
+	while ((sdata[0].IRSensor[0] > irsensor1 && sdata[0].IRSensor[0] <= irsensor2) || (sdata[0].IRSensor[4] <= irsensor6 && sdata[0].IRSensor[4] > irsensor5)) {
+		sdata[0] = Sensor(1);
+		sdata[1] = Sensor(2);
+		end = clock();
+		fprintf(q, "%d\n", sdata[1].MSensor[0]);
+		fprintf(w, "%d\n", sdata[1].MSensor[1]);
+		fprintf(e, "%d\n", end - start);
+		fprintf(r, "%d\n", steer);
+		count = 0;
+		if (a - sdata[1].MSensor[0] <= 5 && a - sdata[1].MSensor[0] >= -5) {
+			count++;
+		}
+		if (b - sdata[1].MSensor[1] <= 5 && b - sdata[1].MSensor[1] >= -5) {
+			count++;
+		}
+		if (count == 2) {
+			x += x_go;
+			y += y_go;
+			map[x][y] = 1;
+		}
+		a = sdata[1].MSensor[0];
+		b = sdata[1].MSensor[1];
+		printf("%d %d %d %d\n", sdata[0].IRSensor[0], sdata[0].IRSensor[4], sdata[0].MSensor[0], sdata[0].MSensor[1]);
 		CDS(speed, sdata);
 	}
 
 	Led(0x0000);
-
-	return sdata;
 }
 
-SensorData right(int speed, SensorData sdata) {
-	int count = 0;
-
+void right(int speed, SensorData sdata[]) {
 	Steering(3);
+	steer = -1;
+	index += 1;
+	if (index > 7) {
+		index = 0;
+	}
+	x_go = xy[index].x;
+	y_go = xy[index].y;
 	Led(0x0010);
 	go(speed);
 
-	while ((sdata.IRSensor[0] >= irsensor1 && sdata.IRSensor[0] <= irsensor3) || sdata.IRSensor[0] <= 0 || (sdata.IRSensor[4] > irsensor5 && sdata.IRSensor[4] <= irsensor4)) {
-		sdata = Sensor(1);
-		if (sdata.IRSensor[0] <= irsensor2 - 6) {           // ï¿½âº»ï¿½ï¿½
+	while ((sdata[0].IRSensor[0] > irsensor2 && sdata[0].IRSensor[0] <= irsensor3) || (sdata[0].IRSensor[4] <= irsensor5 && sdata[0].IRSensor[4] > irsensor5)) {
+		sdata[0] = Sensor(1);
+		sdata[1] = Sensor(2);
+		end = clock();
+		fprintf(q, "%d\n", sdata[1].MSensor[0]);
+		fprintf(w, "%d\n", sdata[1].MSensor[1]);
+		fprintf(e, "%d\n", end - start);
+		fprintf(r, "%d\n", steer);
+		count = 0;
+		if (a - sdata[1].MSensor[0] <= 5 && a - sdata[1].MSensor[0] >= -5) {
 			count++;
 		}
-		if (count >= 5) {
-			go(speed);
-			delay(200);
-			Steering(1);
-			delay(1000);
+		if (b - sdata[1].MSensor[1] <= 5 && b - sdata[1].MSensor[1] >= -5) {
+			count++;
 		}
-		printf("%d %d %d %d\n", sdata.IRSensor[0], sdata.IRSensor[4], sdata.IRSensor[2], sdata.CDSSensor);
-		printf("count : %d\n", count);
+		if (count == 2) {
+			x += x_go;
+			y += y_go;
+			map[x][y] = 1;
+		}
+		a = sdata[1].MSensor[0];
+		b = sdata[1].MSensor[1];
+		printf("%d %d %d %d\n", sdata[0].IRSensor[0], sdata[0].IRSensor[4], sdata[0].MSensor[0], sdata[0].MSensor[1]);
 		CDS(speed, sdata);
 	}
 
 	Led(0x0000);
-
-	return sdata;
 }
 
-void CDS(int speed, SensorData sdata)
+void CDS(int speed, SensorData sdata[])
 {
-	if (sdata.CDSSensor <= 100)
+	int i, j;
+	if (sdata[0].CDSSensor <= 100)
 	{
 		go(0);
-		delay(3000);
-		go(400);
-		delay(1000);
-		go(speed);
+		Led(0x0000);
+		Steering(2);
+
+		Close(szPort);
+
+		for (i = 0; i < 100; i++) {
+			for (j = 0; j < 100; j++) {
+				if (map[i][j] == 1) {
+					printf("1");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+
+		exit(0);
 	}
 }
-SensorData Back(int speed, SensorData sdata) {
-	Steering(2);
-	speed += 50;
+
+void Back(int speed, SensorData sdata[]) {
+	Steering(1);
+	Led(0x0030);
+	Led(0x0040);
+	steer = 1;
+	index -= 1;
+	if (index < 0) {
+		index = 7;
+	}
+	x_go = xy[index].x;
+	y_go = xy[index].y;
 	speed *= -1;
 	go(speed);
 
-	while (sdata.IRSensor[0] > irsensor3 || sdata.IRSensor[4] > irsensor4) {
-		sdata = Sensor(1);
-		printf("%d %d %d %d\n", sdata.IRSensor[0], sdata.IRSensor[4], sdata.IRSensor[2], sdata.CDSSensor);
+	while ((sdata[0].IRSensor[0] > irsensor2 && sdata[0].IRSensor[4] > irsensor5) || sdata[0].IRSensor[0] > irsensor3) {
+		sdata[0] = Sensor(1);
+		sdata[1] = Sensor(2);
+		end = clock();
+		/*
+		fprintf(q, "%d\n", sdata[1].MSensor[0]);
+		fprintf(w, "%d\n", sdata[1].MSensor[1]);*/
+		fprintf(e, "%d\n", end - start);
+		fprintf(r, "%d\n", steer);
+		count = 0;
+		if (a - sdata[1].MSensor[0] <= 5 && a - sdata[1].MSensor[0] >= -5) {
+			count++;
+		}
+		if (b - sdata[1].MSensor[1] <= 5 && b - sdata[1].MSensor[1] >= -5) {
+			count++;
+		}
+		if (count == 2) {
+			x += x_go;
+			y += y_go;
+			map[x][y] = 1;
+		}
+		a = sdata[1].MSensor[0];
+		b = sdata[1].MSensor[1];
+		printf("%d %d %d %d\n", sdata[0].IRSensor[0], sdata[0].IRSensor[4], sdata[0].MSensor[0], sdata[0].MSensor[1]);
+		CDS(-speed, sdata);
 	}
+}
 
-	return sdata;
-void Gobacksound()
-{
-	Sound(44);//ï¿½ï¿½
+void Gobacksound() {
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
+
 	delay(200);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(1000);
 	Sound(0);
 	delay(100);
 
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
-	delay(100);
-	Sound(0);
-	delay(100); 
-	Sound(41);//ï¿½ï¿½
-	delay(100);
-	Sound(0);
-	delay(100); 
-	Sound(41);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(39);//ï¿½ï¿½
+	Sound(41);
+	delay(100);
+	Sound(0);
+	delay(100);
+	Sound(41);
+	delay(100);
+	Sound(0);
+	delay(100);
+	Sound(39);
+
 	delay(400);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
-	delay(500);
-	Sound(0);
-	delay(100); 
-	Sound(41);//ï¿½ï¿½
-	delay(500);
-	Sound(0);
-	delay(100); 
-	Sound(41);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(39);//ï¿½ï¿½
+
+	Sound(41);
+	delay(500);
+	Sound(0);
+	delay(100);
+	Sound(41);
+
+	delay(500);
+	Sound(0);
+	delay(100);
+	Sound(39);
 	delay(2000);
 	Sound(0);
 	delay(100);
-	
-	Sound(44);//ï¿½ï¿½
+
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
+
 	delay(200);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(46);//ï¿½ï¿½
+	Sound(46);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(1000);
 	Sound(0);
 	delay(100);
 
-	Sound(44);//ï¿½ï¿½
+	Sound(44);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(39);//ï¿½ï¿½
+	Sound(39);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(100);
 	Sound(0);
 	delay(100);
-	Sound(37);//ï¿½ï¿½
+	Sound(37);
+
 	delay(300);
-  
+
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(39);//ï¿½ï¿½
+	Sound(39);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(41);//ï¿½ï¿½
+	Sound(41);
 	delay(500);
 	Sound(0);
 	delay(100);
-	Sound(37);//ï¿½ï¿½
+	Sound(37);
 	delay(1500);
 	Sound(0);
 	delay(100);
-  
+
 	Sound(44);
 	delay(100);
 	Sound(0);
@@ -355,4 +520,54 @@ void Gobacksound()
 	Sound(0);
 	delay(100);
 }
+
+void Startsound() {
+
+	Sound(41);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(41);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(41);
+	delay(400);
+	Sound(0);
+	delay(50);
+
+	Sound(41);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(41);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(41);
+	delay(400);
+	Sound(0);
+	delay(50);
+
+	Sound(41);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(44);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(37);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(39);
+	delay(200);
+	Sound(0);
+	delay(50);
+	Sound(41);
+	delay(800);
+	Sound(0);
+	delay(50);
+
 }
